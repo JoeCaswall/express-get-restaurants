efa88432e31316f8b528e3e7a47a3f4956f3ff5d
JoeCaswall <joe.caswall@gmail.com>
2023-11-06 12:35:59 +0000

    part 2 done

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const app = express();
-const Restaurant = require("../models/index")
+const Restaurant = require("../models/index");
 const db = require("../db/connection");
 
-//TODO: Create your GET Request Route Below: 
+//TODO: Create your GET Request Route Below:
 
+app.get("/restaurants", async (req, res) => {
+  const restaurantList = await Restaurant.findAll();
+  res.json(restaurantList);
+});
 
-
+app.get("/restaurants/:id", async (req, res) => {
+  const restaurantID = req.params.id;
+  const particularRestaurant = await Restaurant.findByPk(restaurantID);
+  res.json(particularRestaurant);
+});
 
 module.exports = app;
